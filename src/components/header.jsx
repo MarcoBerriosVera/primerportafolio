0dd654fbc5a17984719ef84b58611d339094ca0a
Your Name <you@example.com>
2025-09-09 17:48:17 -0300

    cambios y correcciones de nombres de variables

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,10 @@
-import style from './header.module.css';
+import styles from './header.module.css';
 
 
 export default function Header() {
   return (
-    <header className={style.header}>
-      <h1 classname={style.title}>Marco Enrique Berrios Vera</h1>
+    <header className={styles.header}>
+      <h1 className={styles.title}>Marco Enrique Berrios Vera</h1>
       <p>Técnico en soportar gente :D</p>
     </header>
   );
--- src/components/profilecard.jsx
+++ src/components/profilecard.jsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
-import styles from './ProfileCard.module.css';
+import styles from './profilecard.module.css';
 
 export default function ProfileCard() {
   return (
     <section>
       <Image
-        className={styles.profileImage}
+        className={styles.profileimage}
         src="/images/logo 3.png" 
         alt="Tu Foto de Perfil"
         width={200}
@@ -13,7 +13,7 @@ export default function ProfileCard() {
       /> 
       <p className={styles.bio}>soy super simpatico, soy lindo pero por dentro :D</p>
       <nav className={styles.socialLinks}>
-        <a href="www.linkedin.com/in/marco-berrios-vera-40845b103">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/marco-berrios-vera-40845b103">LinkedIn</a>
         <a href="https://github.com/MarcoBerriosVera">GitHub</a>
       </nav>
     </section>
